57c0dc199d25d6fde506d2c030e3b9e00ea273dc
Andersonmaunicio <andersonmaunicio@gmail.com>
2026-01-15 22:29:09 -0300

    Classificador de nível de herói finalizado

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
-console.log("Oi");
+let nomeHeroi = "Anderson Lima"
+let xpHeroi = 8000;
+let nivel;
+
+if (xpHeroi < 1000 ) {
+    nivel = "Ferro";
+}
+
+else if (xpHeroi <= 1001) {
+    nivel = "Bronze";
+} 
+
+else if (xpHeroi <= 2001) {
+    nivel = "Prata";
+}
+
+else if (xpHeroi <= 5001) {
+    nivel = "Ouro";
+}
+
+else if (xpHeroi >= 7001) {
+    nivel = "Platina";
+}
+
+
+
+//saída
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
