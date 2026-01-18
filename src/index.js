6bde66ea22da45f79d5595b3edbabf6f6a893b70
Andersonmaunicio <andersonmaunicio@gmail.com>
2026-01-18 10:05:40 -0300

    Finalizando com os Laços de Repetição

--- src/index.js
+++ src/index.js
@@ -1,39 +1,44 @@
 let nomeHeroi = "Anderson Lima"
-let xpHeroi = 8000;
+let xpHeroi = 0;
 let nivel;
 
-if (xpHeroi < 1000 ) {
-    nivel = "Ferro";
+for (let i = 0; i < 10; i++) {
+
+    xpHeroi += 1500; // A cada rodada, ganha +1500 de XP
+
+    if (xpHeroi < 1000) {
+        nivel = "Ferro";
+    }
+
+    else if (xpHeroi <= 2000) {
+        nivel = "Bronze";
+    }
+
+    else if (xpHeroi <= 5000) {
+        nivel = "Prata";
+    }
+
+    else if (xpHeroi <= 7000) {
+        nivel = "Ouro";
+    }
+
+    else if (xpHeroi <= 8000) {
+        nivel = "Platina";
+    }
+
+    else if (xpHeroi <= 9000) {
+        nivel = "Ascendente";
+    }
+
+    else if (xpHeroi <= 10000) {
+        nivel = "Imortal";
+    }
+
+    else if (xpHeroi >= 10001) {
+        nivel = "Radiante";
+    }
+
+
+    //saída
+    console.log("O Herói de nome " + nomeHeroi + " (XP: " + xpHeroi + ") está no nível de " + nivel);
 }
-
-else if (xpHeroi <= 2000) {
-    nivel = "Bronze";
-} 
-
-else if (xpHeroi <= 5000) {
-    nivel = "Prata";
-}
-
-else if (xpHeroi <= 7000) {
-    nivel = "Ouro";
-}
-
-else if (xpHeroi <= 8000) {
-    nivel = "Platina";
-}
-
-else if (xpHeroi <= 9000) {
-    nivel = "Ascendente";
-}
-
-else if (xpHeroi <= 10000) {
-    nivel = "Imortal";
-}
-
-else if (xpHeroi >= 10001) {
-    nivel = "Radiante";
-}
-
-
-//saída
-console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivel);
